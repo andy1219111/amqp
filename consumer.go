a657d81e9979bb1641c01654ca7453ec8e597700
dongbingxu <bxdong@bj-tangseng.com>
2020-12-15 11:58:18 +0800

    增加了生产者类

--- consumer.go
+++ consumer.go
@@ -7,72 +7,69 @@ import (
 	"github.com/streadway/amqp"
 )
 
+//Consumer 消费者对象
 type Consumer struct {
-	uri          string
-	exchangeName string
-	exchangeType string
-	queueName    string
-	routingKey   string
+	QueueName    string
+	ExchangeName string
+	ExchangeType string
+	RoutingKey   string
+	Ch           *amqp.Channel
 	Session      *Session
 	Queue        amqp.Queue
 }
 
-type Session struct {
-	Ch      *amqp.Channel
-	Conn    *amqp.Connection
-	ErrChan chan *amqp.Error
-}
-
-func NewConsumer(uri, exchange, exchangeType, queue, routingKey string) (*Consumer, error) {
+//NewConsumer 得到消费者对象
+func NewConsumer(session *Session, queueName, exchangeName, exchangeType, routingKey string) (*Consumer, error) {
 	consumer := &Consumer{
-		uri:          uri,
-		exchangeName: exchange,
-		exchangeType: exchangeType,
-		queueName:    queue,
-		routingKey:   routingKey,
+		Session:      session,
+		QueueName:    queueName,
+		ExchangeName: exchangeName,
+		ExchangeType: exchangeType,
+		RoutingKey:   routingKey,
 	}
-	session, err := consumer.Connect()
+	channel, err := consumer.Session.Conn.Channel()
+	if err != nil {
+		return nil, err
+	}
+	consumer.Ch = channel
+	//确保rabbitMQ一个一个发送消息
+	err = consumer.Ch.Qos(200, 0, true)
 	if err != nil {
 		return nil, err
 	}
 
-	consumer.Session = session
-	//确保rabbitMQ一个一个发送消息
-	err = consumer.Session.Ch.Qos(200, 0, true)
-	if err != nil {
-		return nil, err
-	}
-	err = session.Ch.ExchangeDeclare(
-		consumer.exchangeName, // name of the exchange
-		consumer.exchangeType, // type
-		true,                  // durable
-		false,                 // delete when complete
-		false,                 // internal
-		false,                 // noWait
+	err = consumer.Ch.ExchangeDeclare(
+		exchangeName, // name of the exchange
+		exchangeType, // type
+		true,         // durable
+		false,        // delete when complete
+		false,        // internal
+		false,        // noWait
 		nil,
 	)
 	if err != nil {
 		return nil, err
 	}
-
-	consumer.Queue, err = session.Ch.QueueDeclare(
-		queue, // name
-		true,  // durable  持久性的,如果事前已经声明了该队列，不能重复声明
-		false, // delete when unused
-		false, // exclusive 如果是真，连接一断开，队列删除
-		false, // no-wait
-		nil,   // arguments
+	consumer.Queue, err = consumer.Ch.QueueDeclare(
+		queueName, // name
+		true,      // durable  持久性的,如果事前已经声明了该队列，不能重复声明
+		false,     // delete when unused
+		false,     // exclusive 如果是真，连接一断开，队列删除
+		false,     // no-wait
+		nil,       // arguments
 	)
 	if err != nil {
-		return consumer, err
+		return nil, err
 	}
+
 	// 队列和交换机绑定，即是队列订阅了发到这个交换机的消息
-	err = session.Ch.QueueBind(
-		consumer.Queue.Name,
+	err = consumer.Ch.QueueBind(
+		queueName,
 		routingKey,
-		exchange,
+		exchangeName,
 		false,
-		nil)
+		nil,
+	)
 	if err != nil {
 		return nil, err
 	}
@@ -80,9 +77,10 @@ func NewConsumer(uri, exchange, exchangeType, queue, routingKey string) (*Consum
 	return consumer, nil
 }
 
+//Consume 消费队列
 func (c *Consumer) Consume() (<-chan amqp.Delivery, error) {
-	msg, err := c.Session.Ch.Consume(
-		c.queueName, // queue
+	msgChan, err := c.Ch.Consume(
+		c.QueueName, // queue
 		"",          // consumer
 		false,       // auto-ack   设置为真自动确认消息
 		false,       // exclusive
@@ -93,54 +91,29 @@ func (c *Consumer) Consume() (<-chan amqp.Delivery, error) {
 	if err != nil {
 		return nil, err
 	}
-	return msg, nil
+	return msgChan, nil
 }
 
-func (c *Consumer) Connect() (*Session, error) {
-	errChan := make(chan *amqp.Error)
-	s := &Session{ErrChan: errChan}
-	conn, err := amqp.Dial(c.uri)
-	if err != nil {
-		return nil, err
-	}
-	go func() {
-		errs := conn.NotifyClose(make(chan *amqp.Error))
-		for err := range errs {
-			s.ErrChan <- err
-		}
-	}()
-
-	channel, err := conn.Channel()
-	if err != nil {
-		return nil, err
-	}
-
-	s.Conn = conn
-	s.Ch = channel
-	return s, err
-}
-
+//ReConnect 重连
 func (c *Consumer) ReConnect() {
 	tikcer := time.NewTicker(time.Second * 3)
 	for range tikcer.C {
-		fmt.Println("start to reconnect....")
 		var err error
-		c.Session.Conn, err = amqp.Dial(c.uri)
+		c.Session.Conn, err = amqp.Dial(c.Session.DSN)
 		if err != nil {
 			fmt.Print(err)
 			continue
 		}
-		c.Session.Ch, err = c.Session.Conn.Channel()
+		c.Ch, err = c.Session.Conn.Channel()
 		if err != nil {
 			fmt.Print(err)
 			continue
 		}
-		fmt.Println("reconnect success....")
 		break
 	}
 }
 
+//Close 关闭consumer
 func (c *Consumer) Close() {
-	c.Session.Ch.Close()
-	c.Session.Conn.Close()
+	c.Ch.Close()
 }
---
+++ producer.go
@@ -0,0 +1,63 @@
+package amqp
+
+import (
+	"github.com/streadway/amqp"
+)
+
+//Producer 生产者
+type Producer struct {
+	ExchangeName string
+	ExchangeType string
+	RoutingKey   string
+	Session      *Session
+	Ch           *amqp.Channel
+}
+
+//NewProducer 得到生产者对象
+func NewProducer(session *Session, exchangeName, exchangeType, routingKey string) (*Producer, error) {
+	producer := &Producer{
+		Session:      session,
+		ExchangeName: exchangeName,
+		ExchangeType: exchangeType,
+		RoutingKey:   routingKey,
+	}
+	channel, err := producer.Session.Conn.Channel()
+	if err != nil {
+		return nil, err
+	}
+	producer.Ch = channel
+
+	err = producer.Ch.ExchangeDeclare(
+		exchangeName, // name of the exchange
+		exchangeType, // type
+		true,         // durable
+		false,        // delete when complete
+		false,        // internal
+		false,        // noWait
+		nil,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return producer, nil
+}
+
+//Publish  消息投递
+func (p *Producer) Publish(routingKey, contentType, body string) error {
+	err := p.Ch.Publish(
+		p.ExchangeName,
+		routingKey,
+		false, false,
+		amqp.Publishing{
+			ContentType:  contentType,
+			Body:         []byte(body),
+			DeliveryMode: amqp.Transient,
+		})
+	return err
+}
+
+//Close  关闭通道
+func (p *Producer) Close() error {
+	return p.Ch.Close()
+}
